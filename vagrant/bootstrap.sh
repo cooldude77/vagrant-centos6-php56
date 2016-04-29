46763080d4826ae8568f7b116d965f23fbc7bcfe
Jim Wood <jim.wood@wearetwogether.com>
2016-04-29 14:28:05 +0100

    =Disabled Nginx support - more work required.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -4,7 +4,7 @@ sudo yum update
 # --- Bootstrap Modules: ---
 sh /vagrant/strapCommands.sh
 sh /vagrant/strapPhp.sh
-sh /vagrant/strapNginx.sh
+# sh /vagrant/strapNginx.sh
 sh /vagrant/strapApache.sh
 sh /vagrant/strapMysql.sh
 sh /vagrant/strapNpm.sh
