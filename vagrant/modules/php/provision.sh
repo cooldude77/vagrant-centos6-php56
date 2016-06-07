db998aaa102b3d25da10dcf74e1b3e036ed82515
Jim Wood <jim.wood@wearetwogether.com>
2016-06-07 13:49:57 +0100

    Housekeeping: remove commented code from php module.

--- vagrant/modules/php/provision.sh
+++ vagrant/modules/php/provision.sh
@@ -8,5 +8,4 @@ sudo rpm -Uvh ius-release*.rpm
 cd /
 rm -rf /root/php-install
 sudo yum -y update
-#sudo yum -y install php56u php56u-opcache php56u-xml php56u-mcrypt php56u-gd php56u-devel php56u-mysql php56u-intl php56u-mbstring php56u-bcmath
 sudo yum -y --skip-broken install php56u php56u-*
